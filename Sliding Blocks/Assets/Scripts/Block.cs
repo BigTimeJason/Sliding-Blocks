9b1b57f1efccebab1ae178370b674f7980e894b4
BigTimeJason <49235257+BigTimeJason@users.noreply.github.com>
2020-12-27 15:29:10 +0000

    Added block moving

---
+++ Sliding Blocks/Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public event System.Action<Block> OnBlockPressed;
+    public Vector2Int coord;
+
+    private void OnMouseDown()
+    {
+        if (OnBlockPressed != null)
+        {
+            OnBlockPressed(this);
+        }
+    }
+
+    public void Init(Vector2Int startingCoord, Texture2D image)
+    {
+        coord = startingCoord;
+
+        GetComponent<MeshRenderer>().material.mainTexture = image;
+        GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Texture");
+    }
+}
