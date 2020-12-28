3d7dcae0930a38e297d81f7edc00e9ac87c2fe5b
BigTimeJason <49235257+BigTimeJason@users.noreply.github.com>
2020-12-28 13:39:20 +0000

    Completed Project
    Shuffle using space
    Completed and Uncomplete states
    Move animation + queueing

--- Sliding Blocks/Assets/Scripts/Block.cs
+++ Sliding Blocks/Assets/Scripts/Block.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public event System.Action<Block> OnBlockPressed;
+    public event System.Action OnFinishedMoving;
+
     public Vector2Int coord;
+    Vector2Int startingCoord;
 
     private void OnMouseDown()
     {
@@ -15,11 +18,39 @@ public class Block : MonoBehaviour
         }
     }
 
+    public void MoveToPosition(Vector2 target, float duration)
+    {
+        StartCoroutine(AnimateMove(target, duration));
+    }
+
     public void Init(Vector2Int startingCoord, Texture2D image)
     {
         coord = startingCoord;
-
+        this.startingCoord = startingCoord;
         GetComponent<MeshRenderer>().material.mainTexture = image;
-        GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Texture");
+        GetComponent<MeshRenderer>().material = Resources.Load<Material>("BlockMaterial");
+    }
+
+    IEnumerator AnimateMove(Vector2 target, float duration)
+    {
+        Vector2 initPos = transform.position;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / duration;
+            transform.position = Vector2.Lerp(initPos, target, percent);
+            yield return null;
+        }
+
+        if(OnFinishedMoving != null)
+        {
+            OnFinishedMoving();
+        }
+    }
+
+    public bool IsAtStartingCoord()
+    {
+        return coord == startingCoord;
     }
 }
